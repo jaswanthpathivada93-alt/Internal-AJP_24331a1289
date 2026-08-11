cf6e819568fa552b3afc1a8a10b0c8f93c16943f
jaswanthpathivada93-alt <jaswanthpathivada93@gmail.com>
2026-08-11 12:26:05 +0530

    Add files via upload

---
+++ Internal.java
@@ -0,0 +1,15 @@
+package mypack;
+public class Calculator {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+    public double division(int a, int b) {
+        return (double) a / b;
+    }
+}
